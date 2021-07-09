81421018a1c3e35e8902b5a66a67ed5697bea5cb
Solo <87192036+developer-solo@users.noreply.github.com>
2021-07-09 16:19:35 +0200

    Create main.lua

---
+++ server/main.lua
@@ -0,0 +1,36 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+function getIdentity(source)
+	local identifier = GetPlayerIdentifiers(source)[1]
+	local result = MySQL.Sync.fetchAll("SELECT firstname,lastname FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
+	if result[1] ~= nil then
+		local identity = result[1]
+
+		return {
+			firstname = identity['firstname'],
+			lastname = identity['lastname'],
+		}
+	else
+		return nil
+	end
+end
+
+ESX.RegisterServerCallback('solo_bodycam:getName', function(source, cb)
+    local name = getIdentity(source)
+    cb(string.upper(name.firstname .. " " .. name.lastname))
+end)
+
+if GetCurrentResourceName() ~= "solo_bodycam" then
+    print("                                             #")
+    print("                                             ###")
+    print("###### ###### ###### ###### ######  ##############")
+    print("#      #    # #    # #    # #    #  ################    Rename the script  '" .. GetCurrentResourceName() .. "' back to  'solo_bodycam'")
+    print("###    ###### ###### #    # ######  ##################  Do not rename the script!")
+    print("#      # ##   # ##   #    # # ##    ################    Why change it when you didn't do it !!!")
+    print("###### #   ## #   ## ###### #   ##  ##############")
+    print("                                             ###")
+    print("                                             #")
+    StopResource(GetCurrentResourceName())
+end
